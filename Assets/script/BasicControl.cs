19531a80d1343fcd7edb406ffd04edb9e2f5fc4f
darkkyu09 <saxjeje@yahoo.com>
2018-03-16 12:26:56 +0700

    Project
    commit pertama,
    progress : mouse look

---
+++ Assets/script/BasicControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicControl : MonoBehaviour {
+
+	public float speed = 3f;
+	private float rotation_x;
+	private float rotation_z;
+
+	// Use this for initialization
+	void Start () {
+
+		rotation_x = GetComponent<CameraPlayerVision> ().GetComponent<Transform> ().eulerAngles.x;
+		rotation_z = GetComponent<CameraPlayerVision> ().GetComponent<Transform> ().eulerAngles.z;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+		
+			transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		}
+
+
+	}
+}
---
+++ Assets/script/CameraPlayerVision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerVision : MonoBehaviour {
+
+	private float s_Horizontal = 2f;
+	private float s_Vertical = 2f;
+	private float yaw = 0f;
+	private float pitch = 0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		yaw += s_Horizontal * Input.GetAxis ("Mouse X");
+		pitch -= s_Vertical * Input.GetAxis ("Mouse Y");
+
+		transform.eulerAngles = new Vector3 (pitch, yaw, 0f);
+
+	}
+
+
+}
